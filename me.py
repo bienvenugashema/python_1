34a773d03ccb3ca969eefee87385a61d7271196a
MWIMULE BIENVENU <129433217+bienvenugashema@users.noreply.github.com>
2025-03-17 17:22:06 +0200

    pushing files to repository

---
+++ me.py
@@ -0,0 +1,8 @@
+
+array = []
+x = 0
+while x < 5:
+    text = input("Enter a text: ").replace(" ", "")
+    array.append(f"{text} => {len(text)}")
+    x += 1
+print(array)
---
+++ new.py
@@ -0,0 +1,7 @@
+
+arr1 = ['a','i','o','e','u']
+arr2 = ['k','j','p','t','f']
+
+name = "Bienvenu"
+
+    
